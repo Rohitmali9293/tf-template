7ff0570681c9d3eebe131661eaa6bb9752bc481a
Rohitmali9293 <rohitmali3010@gmail.com>
2025-03-15 21:54:58 +0530

    update

--- gce/variables.tf
+++ gce/variables.tf
@@ -3,11 +3,9 @@
 variable "project_name" {
   description = "The name of the project to use"
   type        = string
-  default = ""
 }
 
 variable "region" {
   description = "The name of the region to use"
   type        = string
-  default = ""
 }
