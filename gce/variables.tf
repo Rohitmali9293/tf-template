15445c8121c0e84b50dcc656e27d18d61d66063d
Rohitmali9293 <rohitmali3010@gmail.com>
2025-03-15 22:56:50 +0530

    update

--- gce/variables.tf
+++ gce/variables.tf
@@ -3,9 +3,11 @@
 variable "project_name" {
   description = "The name of the project to use"
   type        = string
+  default = "tf-9293-sandbox"
 }
 
 variable "region" {
   description = "The name of the region to use"
   type        = string
+  default = "asia-south1"
 }
