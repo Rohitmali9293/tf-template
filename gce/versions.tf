b4f0dd7d86779530decd87ff64686b76c7b520f6
Rohitmali9293 <rohitmali3010@gmail.com>
2025-03-16 00:29:52 +0530

    update

--- gce/versions.tf
+++ gce/versions.tf
@@ -7,6 +7,11 @@ terraform {
         version = ">= 6.18.0"
         }
    }
+   backend "gcs" {
+    #bucket = "tf-9293-sandbox-terraform-backup"
+    #prefix = "${terraform.workspace}/gce" #Terraform does not support dynamic values in backend.tf
+    #terraform init -backend-config="bucket=tf-9293-sandbox-terraform-backup" -backend-config="prefix=drn/gce"
+  }
  }
 
   provider "google" {
